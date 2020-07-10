c184b4a4d6460353fde34d9ea5617844e2bf9450
Juana <juana_29_1@hotmail.com>
2020-07-10 19:38:47 -0300

    Se crea la funcion borrarTweet para que cada vez que alguien haga un click en botonBorrar el tweet sea eliminado.

--- formularioLS/assets/js/app.js
+++ formularioLS/assets/js/app.js
@@ -6,34 +6,46 @@ const listTweets = document.getElementById('lista-tweets');
 
 eventListeners();
 
-function eventListeners(){
-    //cuando se envia el formulario
-    document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+function eventListeners() {
+     //cuando se envia el formulario
+     document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+
+     //borrar Tweets
+     listTweets.addEventListener('click', borrarTweet);
 }
+
 
 //----------------------- Funciones -------------------------
 
 // Añadir tweet del formulario
-function agregarTweet(e){
-    e.preventDefault();
+function agregarTweet(e) {
+     e.preventDefault();
 
-    const tweet = document.getElementById('tweet').value;
+     const tweet = document.getElementById('tweet').value;
 
-    //creo un nuevo elemento que sera una lista
-    const li = document.createElement('li');
-    //le seteo el texto que ya se escribio en el textTarea
-    li.innerText = tweet;
-    //Se lo paso al elemento padre para poder visualizarlo
-    listTweets.appendChild(li);
+     //creo un nuevo elemento que sera una lista
+     const li = document.createElement('li');
+     //le seteo el texto que ya se escribio en el textTarea
+     li.innerText = tweet;
+     //Se lo paso al elemento padre para poder visualizarlo
+     listTweets.appendChild(li);
 
-    //Creo un boton para borrar
-    const botonBorrar = document.createElement('a');
-    botonBorrar.classList = 'borrar-tweet';
-    botonBorrar.innerText = 'X';
-    // li será el elemento padre de este boton
-    li.appendChild(botonBorrar);
+     //Creo un boton para borrar
+     const botonBorrar = document.createElement('a');
+     botonBorrar.classList = 'borrar-tweet';
+     botonBorrar.innerText = 'X';
+     // li será el elemento padre de este boton
+     li.appendChild(botonBorrar);
 
-    //una vez agregado a la lista limpio el textTarea
-    document.getElementById('tweet').value='';
+     //una vez agregado a la lista limpio el textTarea
+     document.getElementById('tweet').value = '';
 
+}
+
+function borrarTweet(e) {
+     e.preventDefault();
+
+     if (e.target.className === 'borrar-tweet') {
+          e.target.parentElement.remove();
+     }
 }
